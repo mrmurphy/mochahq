3d962d6d187ed3fdc922c13678984f99e778a468
Murphy Randle <murphyrandle@gmail.com>
2016-04-14 19:14:26 -0600

    Augment mocha definition

--- flow-typed/mocha.js
+++ flow-typed/mocha.js
@@ -2,5 +2,6 @@
 
 declare function describe(name:string, callback:Function):void;
 declare function before(callback:Function):void;
+declare function after(callback:Function):void;
 declare function beforeEach(callback:Function):void;
 declare function it(name:string, callback:Function):void;
